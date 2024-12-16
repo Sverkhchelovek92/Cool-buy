63014244f2692ca153d7a4759337b2de7fd1c70d
Miroslav Gurnyak <mirmiroslav@gmail.com>
2024-12-16 16:44:13 +0300

    Currency change in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Card({ product }) {
+  const { currentCurrency } = useSelector((state) => state.currency)
+
+  const exchangeRates = {
+    USD: 1,
+    EUR: 0.9,
+    RUB: 90,
+  }
+
+  const convertedPrice = (
+    product.price * exchangeRates[currentCurrency]
+  ).toFixed(2)
+
   return (
     <div className="card">
       <div className="card-img-block">
@@ -14,7 +27,7 @@ function Card({ product }) {
       <h3 className="card-name">{product.name}</h3>
       <p className="card-description">{product.description}</p>
       <p className="card-price">
-        <strong>Price:</strong> ${product.price}
+        <strong>Price:</strong> {convertedPrice} {currentCurrency}
       </p>
       <button className="card-btn">Buy</button>
     </div>
