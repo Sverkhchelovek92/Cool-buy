76e6bfc5ff921c5d16b9aef4613ed313c36b7f6b
Miroslav Gurnyak <mirmiroslav@gmail.com>
2024-12-20 12:19:57 +0300

    cart and currency stuff

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,7 +4,23 @@ import { clearCart, removeFromCart } from '../features/cartSlice'
 
 function Cart() {
   const cartItems = useSelector((state) => state.cart.items)
+
+  const { currentCurrency } = useSelector((state) => state.currency)
+
+  const exchangeRates = {
+    USD: 1,
+    EUR: 0.9,
+    RUB: 90,
+  }
+
   const dispatch = useDispatch()
+
+  const total = cartItems.reduce((sum, item) => {
+    return (
+      sum +
+      (item.price * exchangeRates[currentCurrency]).toFixed(2) * item.quantity
+    )
+  }, 0)
 
   const handleRemove = (id) => {
     dispatch(removeFromCart(id))
@@ -24,11 +40,18 @@ function Cart() {
           <ul className="cart-list">
             {cartItems.map((item) => (
               <li className="cart-item" key={item.id}>
-                {item.name} - {item.price} x {item.quantity}
+                {item.name} -{' '}
+                {(item.price * exchangeRates[currentCurrency]).toFixed(2)}{' '}
+                {currentCurrency} x {item.quantity}
                 <button onClick={() => handleRemove(item.id)}>Delete</button>
               </li>
             ))}
           </ul>
+        )}
+        {cartItems.length > 0 && (
+          <p>
+            Total: {total.toFixed(2)} {currentCurrency}{' '}
+          </p>
         )}
         {cartItems.length > 0 && (
           <button onClick={handleClear}>Clear Cart</button>
