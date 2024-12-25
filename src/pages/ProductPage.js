2ff17f8bd67fa839fcdd4275db1d743b17eed950
Miroslav Gurnyak <mirmiroslav@gmail.com>
2024-12-25 09:53:52 +0300

    product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import ProductData from '../data/ProductData'
 
 function ProductPage() {
   const { id } = useParams()
   const product = ProductData.find((item) => item.id === parseInt(id))
+
+  //
+  const { currentCurrency } = useSelector((state) => state.currency)
+
+  const exchangeRates = {
+    USD: 1,
+    EUR: 0.9,
+    RUB: 90,
+  }
+
+  //
 
   if (!product) {
     return (
@@ -17,9 +29,23 @@ function ProductPage() {
   return (
     <div className="main">
       <div className="wrapper">
-        <h1>{product.name}</h1>
-        <p>{product.price}</p>
-        <p>{product.description}</p>
+        <div className="product-wrapper">
+          <div className="product-img-block">
+            <img
+              src={require(`../data/img/${product.image}`)}
+              className="product-img-img"
+              alt={product.name}
+            />
+          </div>
+          <div className="product-info">
+            <h1>{product.name}</h1>
+            <h3>
+              {(product.price * exchangeRates[currentCurrency]).toFixed(2)}
+              {currentCurrency}
+            </h3>
+            <p>{product.description}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
